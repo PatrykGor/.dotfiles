1309aefcc1bec571102455f872c38a6b045e6fe6
PatrykGor <patryk@gorscy.net>
2022-04-14 22:00:30 +0200

    Added mkSystemUser

--- lib/user.nix
+++ lib/user.nix
@@ -1,24 +1,25 @@
 { pkgs, home-manager, lib, system, overlays, ... }:
 with builtins;
 {
-  mkHMUser = {userConfig, username}:
+  mkHMUser = { userConfig, username }:
     home-manager.lib.homeManagerConfiguration {
       inherit system username pkgs;
       stateVersion = "21.05";
       configuration =
         let
           trySettings = tryEval (fromJSON (readFile /etc/hmsystemdata.json));
-          machineData = if trySettings.success then trySettings.value else {};
+          machineData = if trySettings.success then trySettings.value else { };
 
           machineModule = { pkgs, config, lib, ... }: {
             options.machineData = lib.mkOption {
-              default = {};
+              default = { };
               description = "Settings passed from nixos system configuration. If not present will be empty";
             };
 
             config.machineData = machineData;
           };
-        in {
+        in
+        {
           patryk = userConfig;
 
           nixpkgs.overlays = overlays;
@@ -32,5 +33,18 @@ with builtins;
           imports = [ ../modules/users machineModule ];
         };
       homeDirectory = "/home/${username}";
+    };
+
+  mkSystemUser = { name, groups, uid, shell, ... }:
+    {
+      users.users."${name}" = {
+        name = name;
+        isNormalUser = true;
+        isSystemUser = false;
+        extraGroups = groups;
+        uid = uid;
+        initialPassword = "changeme";
+        shell = shell;
+      };
     };
 }
